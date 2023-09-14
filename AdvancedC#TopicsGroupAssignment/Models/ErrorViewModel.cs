b3473e23326a4c2e6cd5679a36d64d78a027fa84
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-09-14 10:31:14 -0500

    Add project files.

---
+++ AdvancedC#TopicsGroupAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedC_TopicsGroupAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
